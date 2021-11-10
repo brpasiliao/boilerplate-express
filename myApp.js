f4fb619d5b1ee391d761a8bba8d63911aef5a9c1
Bian <brpasiliao@gmail.com>
2021-11-10 18:46:03 -0500

    served public directory

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use("/public", express.static(__dirname + "/public"));
+
 console.log("Hello World");
 
 module.exports = app;
