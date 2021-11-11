7aebbb8431465b88c550f444417f9e83883063f1
Bian <brpasiliao@gmail.com>
2021-11-10 19:58:25 -0500

    dont know why its not working

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var express = require("express");
 var app = express();
 
@@ -7,8 +9,8 @@ app.get("/", function (req, res) {
 
 app.get("/json", function (req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase")
-    res.json({ message: "Hello json" });
-  else res.json({ message: "HELLO JSON" });
+    res.json({ message: "HELLO JSON" });
+  else res.json({ message: "Hello json" });
 });
 
 app.use("/public", express.static(__dirname + "/public"));
