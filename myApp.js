088cf98184f8f72de14d2e6a915723895fe34ec3
Bian <brpasiliao@gmail.com>
2021-11-10 19:29:32 -0500

    created .env file

--- myApp.js
+++ myApp.js
@@ -6,7 +6,9 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", function (req, res) {
-  res.json({ message: "Hello json" });
+  if (process.env.MESSAGE_STYLE == "uppercase")
+    res.json({ message: "Hello json" });
+  else res.json({ message: "HELLO JSON" });
 });
 
 app.use("/public", express.static(__dirname + "/public"));
