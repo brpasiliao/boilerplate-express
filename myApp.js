769063a824218ba647b6bd5e3fb8dc4bebbe3c07
brpasiliao <brpasiliao@gmail.com>
2021-12-11 02:52:05 +0000

    finished exercise

--- myApp.js
+++ myApp.js
@@ -1,16 +1,56 @@
-require("dotenv").config();
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.route("/name")
+  .get((req, res) => {
+    var response = req.query.first + " " + req.query.last;
+    res.send({ name: response });
+  })
+  .post((req, res) => {
+    var response = req.body.first + " " + req.body.last;
+    res.send({ name: response });
+  })
+
+// GET /:word/echo
+app.get("/:word/echo", (req, res) => {
+  res.send({ echo: req.params.word });
 });
 
-app.get("/json", function (req, res) {
-  if (process.env.MESSAGE_STYLE == "uppercase")
-    res.json({ message: "HELLO JSON" });
-  else res.json({ message: "Hello json" });
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
+app.get("/json", (req, res) => {
+  var response = "Hello json";
+  const mySecret = process.env['MESSAGE_STYLE'];
+  
+  if (mySecret === "uppercase") 
+    res.json({ message: response.toUpperCase() });
+  else res.json({ message: response });
+});
+
+app.get("/", function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
